4740ef85e3f4e535cf3e1f552bd52ef81e3a356a
Paul Hindt <phindt@gmail.com>
2023-01-31 02:07:41 -0800

    Add dlopen/dlclose/dlsym functions
    Load d3d11 and dxgi.dll modules

--- main.c
+++ main.c
@@ -1,5 +1,38 @@
 #include <Windows.h>
 #include <d3d11.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void *os_dlopen(const char* path)
+{
+    if (!path)
+        return NULL;
+
+    size_t len = strlen(path) + 1;
+    wchar_t* path_wide = (wchar_t*)malloc((sizeof(wchar_t) * len));
+    size_t conv_len = 0;
+    size_t wlen = MultiByteToWideChar(CP_ACP, 0, path, (int)len, 0, 0); 
+    MultiByteToWideChar(CP_ACP, 0, path, (int)len, path_wide, (int)wlen);
+
+    HMODULE module = LoadLibrary(path_wide);
+    if (!module)
+        return NULL;
+
+    free((void*)path_wide);
+    path_wide = NULL;
+
+    return (void*)module;
+}
+
+void* os_dlsym(void* module, const char* func)
+{
+    return GetProcAddress(module, func);
+}
+
+void os_dlclose(void* module)
+{
+    FreeLibrary(module);
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
@@ -17,6 +50,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE instance_prev, PWSTR cmd_line, int cmd_show)
 {
+    HMODULE d3d11_dll = (HMODULE)os_dlopen("d3d11.dll");
+    if (!d3d11_dll) {
+        return 1;
+    }
+    HMODULE dxgi_dll = (HMODULE)os_dlopen("dxgi.dll");
+    if (!dxgi_dll) {
+        return 1;
+    }
+
     // Register the window class.
     const wchar_t class_name[]  = L"D3D11 in C: Lesson 1";
 
